e88733d063afb5cc8be661309d7f7f03239212a8
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-16 12:40:31 +0200

    feat(scripts): add code formatting script
    Added scripts/format.sh to format the code in the source and test directories using autopep8.
    
    - Files Added:
      - scripts/format.sh
    - Purpose: To ensure code style consistency by formatting code in the source and test directories.
    - Features:
      - Sources the configuration script to use defined variables.
      - Activates the virtual environment.
      - Formats code in the source and test directories using autopep8 with aggressive settings.
    - Impact: Enhances code readability and maintainability by enforcing consistent code style.

---
+++ scripts/format.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ========================================================
+# scripts/format.sh
+# ========================================================
+# Description: This script formats the code in the source and test
+# directories using autopep8 with aggressive settings.
+#
+# Usage: This script is intended to be run by the Makefile and can
+# also be run manually to ensure code style consistency.
+#
+# Repository: https://github.com/jekwwer/python-template
+# Author: Evgenii Shiliaev
+# Date: 2024-07-16
+# ========================================================
+
+# Source the configuration script
+source "$(dirname "$0")/config.sh"
+
+# Use the variables defined in config.sh
+source $VENV_DIR/bin/activate
+
+# Format code in the source and test directories
+$VENV_DIR/bin/autopep8 --in-place --aggressive --aggressive --recursive $SRC_DIR/ $TEST_DIR/
+
+# ========================================================
+# End of scripts/format.sh
+# ========================================================
