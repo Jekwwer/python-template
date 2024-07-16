52ba7711a0ff55176ff47ce2f9dea591a5f30d24
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-16 14:04:28 +0200

    feat(scripts): add security checking script
    Added scripts/security_check.sh to perform security checks on the code in the source and test directories using Bandit.
    
    - Files Added:
      - scripts/security_check.sh
    - Purpose: To identify and address potential security issues through static analysis of the source and test directories.
    - Features:
      - Sources the configuration script to use defined variables.
      - Activates the virtual environment.
      - Performs security checks using Bandit.
    - Impact: Enhances code security by detecting common vulnerabilities and promoting best practices in security.

---
+++ scripts/security_check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ========================================================
+# scripts/security_check.sh
+# ========================================================
+# Description: This script performs security checks on the code in the source
+# and test directories using Bandit to identify potential security issues.
+#
+# Usage: This script is intended to be run by the Makefile and can also
+# be run manually to ensure the codebase is free from common security vulnerabilities.
+#
+# Repository: https://github.com/jekwwer/python-template
+# Author: Evgenii Shiliaev
+# Date: 2024-07-16
+# ========================================================
+
+# Source the configuration script
+source "$(dirname "$0")/config.sh"
+
+# Use the variables defined in config.sh
+source $VENV_DIR/bin/activate
+
+# Perform security checks using Bandit
+$VENV_DIR/bin/bandit -r $SRC_DIR/ $TEST_DIR/
+
+# ========================================================
+# End of scripts/security_check.sh
+# ========================================================
