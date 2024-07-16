6c8a938551710dcb923bc67a17596db6c9b1704d
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-16 14:03:26 +0200

    feat(scripts): add static type checking script
    Added scripts/type_check.sh to perform static type checking on the code in the source and test directories using Mypy.
    
    - Files Added:
      - scripts/type_check.sh
    - Purpose: To verify type annotations and detect type-related errors through static type checking of the source and test directories.
    - Features:
      - Sources the configuration script to use defined variables.
      - Activates the virtual environment.
      - Performs static type checking using Mypy.
    - Impact: Enhances code reliability and maintainability by ensuring type correctness and identifying potential type-related issues early in the development process.

---
+++ scripts/type_check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ========================================================
+# scripts/type_check.sh
+# ========================================================
+# Description: This script performs static type checking on the code in the
+# source and test directories using Mypy to ensure type correctness.
+#
+# Usage: This script is intended to be run by the Makefile and can also
+# be run manually to verify type annotations and detect type-related errors.
+#
+# Repository: https://github.com/jekwwer/python-template
+# Author: Evgenii Shiliaev
+# Date: 2024-07-16
+# ========================================================
+
+# Source the configuration script
+source "$(dirname "$0")/config.sh"
+
+# Use the variables defined in config.sh
+source $VENV_DIR/bin/activate
+
+# Perform static type checking using Mypy
+$VENV_DIR/bin/mypy $SRC_DIR/ $TEST_DIR/
+
+# ========================================================
+# End of scripts/type_check.sh
+# ========================================================
