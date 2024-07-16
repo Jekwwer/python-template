54de35824a2b4dec3be557a290dd871b717dfe6a
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-16 14:01:26 +0200

    feat(scripts): add code linting script
    Added scripts/lint.sh to perform static analysis on the code in the source and test directories using Flake8 and Pylint.
    
    - Files Added:
      - scripts/lint.sh
    - Purpose: To check for coding standard violations and potential errors through static analysis of the source and test directories.
    - Features:
      - Sources the configuration script to use defined variables.
      - Activates the virtual environment.
      - Performs static analysis using Flake8 and Pylint.
    - Impact: Enhances code quality by identifying and reporting coding standard violations and potential errors, promoting best practices and maintainability.

---
+++ scripts/lint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ========================================================
+# scripts/lint.sh
+# ========================================================
+# Description: This script performs static analysis on the code in the source
+# and test directories using Flake8 and Pylint to ensure code quality.
+#
+# Usage: This script is intended to be run by the Makefile and can also
+# be run manually to check for coding standard violations and potential errors.
+#
+# Repository: https://github.com/jekwwer/python-template
+# Author: Evgenii Shiliaev
+# Date: 2024-07-16
+# ========================================================
+
+# Source the configuration script
+source "$(dirname "$0")/config.sh"
+
+# Use the variables defined in config.sh
+source $VENV_DIR/bin/activate
+
+# Perform static analysis using Flake8 and Pylint
+$VENV_DIR/bin/flake8 $SRC_DIR/ $TEST_DIR/
+$VENV_DIR/bin/pylint $SRC_DIR/ $TEST_DIR/
+
+# ========================================================
+# End of scripts/lint.sh
+# ========================================================
