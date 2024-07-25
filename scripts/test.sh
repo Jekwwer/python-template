7b537368ed2927a4e9d6b14686ef0769f60f0b5f
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-25 13:07:19 +0200

    test(tox): add Tox configuration for testing and update test setup
    Added a `tox.ini` file to configure Tox for managing virtual environments and running tests. Updated `requirements.txt` to include Tox and modified `scripts/test.sh` to use Tox for running tests.
    
    - Files Added:
      - tox.ini
    - Files Modified:
      - requirements.txt
      - scripts/test.sh
    - Purpose: To streamline the testing process by using Tox for environment management and test execution, ensuring consistent testing across different environments.
    - Features:
      - `tox.ini`:
        - Configures Tox to use Python 3.10.
        - Sets up dependencies for testing with pytest and coverage.
        - Defines commands to run tests and generate coverage reports.
      - `requirements.txt`:
        - Added `tox` to manage testing environments and dependencies.
      - `scripts/test.sh`:
        - Updated to run tests using Tox instead of directly invoking pytest.
    - Impact: Improves the consistency and reliability of the testing process by standardizing environment setup and test execution through Tox.

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,25 +2,22 @@
 # ========================================================
 # scripts/test.sh
 # ========================================================
-# Description: This script runs the test suite using pytest to ensure that
-# all tests in the test directory pass successfully.
+# Description: This script runs the test suite using pytest via Tox
+# to ensure that all tests in the test directory pass successfully.
 #
 # Usage: This script is intended to be run by the Makefile and can also
 # be run manually to execute the test suite.
 #
 # Repository: https://github.com/jekwwer/python-template
 # Author: Evgenii Shiliaev
-# Date: 2024-07-24
+# Date: 2024-07-25
 # ========================================================
 
-# Source the configuration script
-source "$(dirname "$0")/config.sh"
+# Install Tox because it needs to run in the same shell on the CI server
+pip install tox
 
-# Use the variables defined in config.sh
-source $VENV_DIR/bin/activate
-
-# Run the test suite using pytest and generate a coverage report
-$VENV_DIR/bin/pytest --cov=$SRC_DIR --cov-report xml:coverage.xml $TEST_DIR
+# Run tests with Tox
+tox
 
 # ========================================================
 # End of scripts/test.sh
