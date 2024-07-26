07f03a1864ef5ae68e37cdc6461feecadcd5602a
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-26 15:00:10 +0200

    chore(test): update test script for timestamped coverage report
    Updated `scripts/test.sh` to use the configuration script for setting up environment variables and renamed the coverage report file to include a timestamp. Also added usage instructions in the script header.
    
    - Files Modified:
      - scripts/test.sh
    - Purpose: To ensure consistency in environment setup and report naming, and to provide usage instructions for the test script.
    - Changes:
      - Added sourcing of `config.sh` to use predefined environment variables, including paths and report filenames.
      - Renamed the coverage report file to include a timestamp for better tracking and organization.
      - Added usage instructions to the script header, including how to run the script using `make test`.
    - Impact: This update ensures that the coverage report is uniquely identified with a timestamp, aiding in the organization and tracking of test runs. It also standardizes the setup process by sourcing the configuration script.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,21 +3,30 @@
 # scripts/test.sh
 # ========================================================
 # Description: This script runs the test suite using pytest via Tox
-# to ensure that all tests in the test directory pass successfully.
+#              to ensure that all tests in the test directory pass successfully.
 #
-# Usage: This script is intended to be run by the Makefile and can also
-# be run manually to execute the test suite.
+# Usage: This script is intended to be run by the Makefile
+#        and can also be run manually to execute the test suite.
+#
+# Run the script using:
+# make test
 #
 # Repository: https://github.com/jekwwer/python-template
 # Author: Evgenii Shiliaev
-# Date: 2024-07-25
+# Date: 2024-07-26
 # ========================================================
+
+# Source the configuration script
+source "$(dirname "$0")/config.sh"
 
 # Install Tox because it needs to run in the same shell on the CI server
 pip install tox
 
 # Run tests with Tox
 tox
+
+# Rename the coverage report file to have a timestamp
+mv $REPORTS_DIR/coverage.xml $COVERAGE_REPORT
 
 # ========================================================
 # End of scripts/test.sh
