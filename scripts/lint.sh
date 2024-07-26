d86140ca826715d3026471e0c458982256e920bc
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-26 15:36:25 +0200

    chore(lint): redirect flake8 and pylint outputs to log files and update script header
    Updated `scripts/lint.sh` to capture the outputs of flake8 and pylint into log files. Also, improved the script header for clarity and included usage instructions.
    
    - Files Modified:
      - scripts/lint.sh
    - Purpose: To log detailed outputs from linting tools (flake8 and pylint) for better tracking and debugging of linting issues, and to enhance the documentation and usability of the script.
    - Changes:
      - Redirected flake8 output to a log file specified by the `FLAKE8_REPORT` variable.
      - Redirected pylint output to a log file specified by the `PYLINT_REPORT` variable.
      - Indented and formatted the header to include consistent sections for `Description:` and `Usage:`.
      - Added usage instructions to the script header, specifying how to run the script using `make lint`.
    - Impact: This change ensures that all linting outputs are captured and stored, facilitating easier review and debugging. It also improves the script's documentation, making it more user-friendly.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,15 +2,20 @@
 # ========================================================
 # scripts/lint.sh
 # ========================================================
-# Description: This script performs static analysis on the code in the source
-# and test directories using Flake8 and Pylint to ensure code quality.
+# Description: This script performs static analysis on the code
+#              in the source and test directories using
+#              Flake8 and Pylint to ensure code quality.
 #
-# Usage: This script is intended to be run by the Makefile and can also
-# be run manually to check for coding standard violations and potential errors.
+# Usage: This script is intended to be run by the Makefile
+#        and can also be run manually to check
+#        for coding standard violations and potential errors.
+#
+# Run the script using:
+# make lint
 #
 # Repository: https://github.com/jekwwer/python-template
 # Author: Evgenii Shiliaev
-# Date: 2024-07-16
+# Date: 2024-07-26
 # ========================================================
 
 # Source the configuration script
@@ -20,8 +25,8 @@ source "$(dirname "$0")/config.sh"
 source $VENV_DIR/bin/activate
 
 # Perform static analysis using Flake8 and Pylint
-$VENV_DIR/bin/flake8 $SRC_DIR/ $TEST_DIR/
-$VENV_DIR/bin/pylint $SRC_DIR/ $TEST_DIR/
+$VENV_DIR/bin/flake8 $SRC_DIR/ $TEST_DIR/ --output-file=$FLAKE8_REPORT
+$VENV_DIR/bin/pylint $SRC_DIR/ $TEST_DIR/ >$PYLINT_REPORT
 
 # ========================================================
 # End of scripts/lint.sh
