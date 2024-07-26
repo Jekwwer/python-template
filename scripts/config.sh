db8c7cd1742eda47fa3d863a82b1a48b366b5458
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-26 13:36:22 +0200

    chore(config): ensure reports directory exists and set reports path
    Updated `scripts/config.sh` to define the `REPORTS_DIR` variable and ensure the directory is created if it doesn't exist, providing a centralized location for storing analysis outputs.
    
    - Files Modified:
      - scripts/config.sh
    - Purpose: To standardize the location for storing reports generated by analysis scripts and to ensure the directory is available, preventing errors due to missing directories.
    - Changes:
      - Defined `REPORTS_DIR=reports` to specify the path for storing reports.
      - Added a command to create the `reports` directory using `mkdir -p $REPORTS_DIR` if it does not already exist.
    - Impact: This update ensures a consistent and error-free setup for generating and storing analysis reports, enhancing the organization and maintenance of project outputs.

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,14 +4,15 @@
 # ========================================================
 # Description: This script defines shared variables used across
 # different scripts in the project. These variables include paths
-# to the virtual environment, source directory, and test directory.
+# to the virtual environment, source directory, test directory,
+# reports directory.
 #
 # Usage: This script is intended to be sourced by other scripts
 # to maintain consistency and avoid repetition.
 #
 # Repository: https://github.com/jekwwer/python-template
 # Author: Evgenii Shiliaev
-# Date: 2024-07-16
+# Date: 2024-07-26
 # ========================================================
 
 # Define shared variables
@@ -19,6 +20,10 @@ PYTHON=python3
 VENV_DIR=venv
 SRC_DIR=src
 TEST_DIR=tests
+REPORTS_DIR=reports
+
+# Create reports directory if it doesn't exist
+mkdir -p $REPORTS_DIR
 
 # ========================================================
 # End of scripts/config.sh
