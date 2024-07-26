8163b6395fde5a006c5e479135bdc8a99843aab3
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-26 15:42:00 +0200

    chore(type-check): redirect mypy output to log file and update script header
    Updated `scripts/type_check.sh` to capture the output of mypy type checking into a log file. Also improved the script header for clarity and included usage instructions.
    
    - Files Modified:
      - scripts/type_check.sh
    - Purpose: To log the detailed output from mypy type checking for easier review and debugging, and to enhance the documentation and usability of the script.
    - Changes:
      - Redirected mypy output to a log file specified by the `MYPY_REPORT` variable.
      - Indented and formatted the header to include consistent sections for `Description:` and `Usage:`.
      - Added usage instructions to the script header, specifying how to run the script using `make type-check`.
    - Impact: This change ensures that all type checking outputs are logged, facilitating easier review and debugging of type-related issues. The improved script header provides better documentation and usage guidance.

--- scripts/type_check.sh
+++ scripts/type_check.sh
@@ -2,15 +2,20 @@
 # ========================================================
 # scripts/type_check.sh
 # ========================================================
-# Description: This script performs static type checking on the code in the
-# source and test directories using Mypy to ensure type correctness.
+# Description: This script performs static type checking on the code
+#              in the source and test directories using Mypy
+#              to ensure type correctness.
 #
-# Usage: This script is intended to be run by the Makefile and can also
-# be run manually to verify type annotations and detect type-related errors.
+# Usage: This script is intended to be run by the Makefile
+#        and can also be run manually to verify type annotations
+#        and detect type-related errors.
+#
+# Run the script using:
+# make type-check
 #
 # Repository: https://github.com/jekwwer/python-template
 # Author: Evgenii Shiliaev
-# Date: 2024-07-16
+# Date: 2024-07-26
 # ========================================================
 
 # Source the configuration script
@@ -20,7 +25,7 @@ source "$(dirname "$0")/config.sh"
 source $VENV_DIR/bin/activate
 
 # Perform static type checking using Mypy
-$VENV_DIR/bin/mypy $SRC_DIR/ $TEST_DIR/
+$VENV_DIR/bin/mypy $SRC_DIR/ $TEST_DIR/ >$MYPY_REPORT
 
 # ========================================================
 # End of scripts/type_check.sh
