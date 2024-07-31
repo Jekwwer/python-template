c0253964bcbd01c8830ea0aaf0eeb965eea94494
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-07-31 23:16:50 +0200

    feat(lint): add error handling and logging for Flake8 and Pylint
    Enhanced `scripts/lint.sh` with error handling and logging for Flake8 and Pylint. These changes ensure that any issues detected during static analysis are logged and reported.
    
    - Files Modified:
      - scripts/lint.sh
    - Purpose: To log and handle errors during static analysis with Flake8 and Pylint, ensuring that issues are properly captured and reported for further investigation.
    - Changes:
      - Added error checking after running Flake8 and Pylint. If either command fails (non-zero exit code), an error message is logged using the `log_error()` function.
      - The error messages include references to the respective log files (`FLAKE8_REPORT` and `PYLINT_REPORT`) for detailed error information.
      - Included a call to `exit_check()` at the end of the script to assess the overall success of the linting process and exit accordingly.
    - Impact: This update improves error detection and reporting for linting processes, making it easier to identify and address issues in the codebase.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -15,7 +15,7 @@
 #
 # Repository: https://github.com/jekwwer/python-template
 # Author: Evgenii Shiliaev
-# Date: 2024-07-26
+# Date: 2024-07-31
 # ========================================================
 
 # Source the configuration script
@@ -24,10 +24,19 @@ source "$(dirname "$0")/config.sh"
 # Use the variables defined in config.sh
 source $VENV_DIR/bin/activate
 
-# Perform static analysis using Flake8 and Pylint
+# Perform static analysis using Flake8
 $VENV_DIR/bin/flake8 $SRC_DIR/ $TEST_DIR/ --output-file=$FLAKE8_REPORT
-$VENV_DIR/bin/pylint $SRC_DIR/ $TEST_DIR/ >$PYLINT_REPORT
+if [ $? -ne 0 ]; then
+    log_error "flake8 failed. Check $FLAKE8_REPORT for details."
+fi
 
+# Perform static analysis using Pylint
+$VENV_DIR/bin/pylint $SRC_DIR/ $TEST_DIR/ >$PYLINT_REPORT
+if [ $? -ne 0 ]; then
+    log_error "pylint failed. Check $PYLINT_REPORT for details."
+fi
+
+exit_check
 # ========================================================
 # End of scripts/lint.sh
 # ========================================================
