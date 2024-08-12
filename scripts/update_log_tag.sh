240c25b989a32801fd57bbf7ea4836f6cd403e48
Evgenii Shiliaev <evgenii.shiliaev.work@pm.me>
2024-08-12 04:33:33 +0200

    fix(ci): skip log tag updates on GitHub Actions to prevent sonar issues
    Changes:
    - `scripts/update_log_tag.sh`:
      - Added a check to skip log tag updates when running on GitHub Actions by verifying if the `GITHUB_ACTIONS` environment variable is set to `true`.
      - This prevents the log tag from being overwritten, ensuring that SonarCloud can properly detect the files after the CI build.
    
    Impact:
    - Resolves issues where SonarCloud fails to find files due to log tag overwriting on GitHub Actions.

--- scripts/update_log_tag.sh
+++ scripts/update_log_tag.sh
@@ -16,11 +16,16 @@
 # Author: Evgenii Shiliaev
 # Author's GitHub Username: @Jekwwer
 #
-# Date: 2024-08-10
+# Date: 2024-08-12
 # ========================================================
 
 # Source the configuration script
 source "$(dirname "$0")/config.sh"
+
+# Check if running on GitHub Actions
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    exit_check 0 # Skip this script on GitHub Actions
+fi
 
 # Check if the log tag file exists and delete it if found
 execute_silently "[ -f \"$LOG_TAG_FILE\" ] && rm -f \"$LOG_TAG_FILE\"" "check and delete log tag file"
